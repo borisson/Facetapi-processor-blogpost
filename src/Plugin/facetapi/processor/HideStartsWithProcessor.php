57e68f31feba0fd2d87ce89feb69ccc884e2f156
Joris Vercammen <botchcake@gmail.com>
2015-11-22 15:24:07 +0100

    extended config for processor

--- src/Plugin/facetapi/processor/HideStartsWithProcessor.php
+++ src/Plugin/facetapi/processor/HideStartsWithProcessor.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\hide_starts_with\Plugin\facetapi\processor;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\facetapi\FacetInterface;
 use Drupal\facetapi\Processor\BuildProcessorInterface;
 use Drupal\facetapi\Processor\ProcessorPluginBase;
@@ -19,11 +20,37 @@ use Drupal\facetapi\Processor\ProcessorPluginBase;
  * )
  */
 class HideStartsWithProcessor extends ProcessorPluginBase implements BuildProcessorInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state, FacetInterface $facet) {
+    $processors = $facet->getProcessors();
+    $config = isset($processors[$this->getPluginId()]) ? $processors[$this->getPluginId()] : null;
+
+    $build['character'] = [
+      '#title' => $this->t('Character to hide'),
+      '#type' => 'textfield',
+      '#default_value' => !is_null($config) ? $config->getConfiguration()['character'] : $this->defaultConfiguration()['exclude'],
+      '#description' => $this->t("All results that start with this character will be hidden."),
+    ];
+
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function build(FacetInterface $facet, array $results) {
+
+    $processors = $facet->getProcessors();
+    $config = $processors[$this->getPluginId()];
+
+    $char = $config->getConfiguration()['character'];
 
     /** @var \Drupal\facetapi\Result\ResultInterface $result */
     foreach ($results as $id => $result) {
-      if (strpos(strtolower($result->getDisplayValue()), 'a') === 0) {
+      if (strpos(strtolower($result->getDisplayValue()), $char) === 0) {
         unset($results[$id]);
       }
     }
